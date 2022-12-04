256892a9ff63320d0ae8ad4e5af887a173e1d00d
CutmanCometh <sleepingbruin@gmail.com>
2022-12-03 22:26:15 -0700

    josh/day 4

---
+++ josh/4.1/index.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+
+const assignmentPairsText = fs.readFileSync('assignment_pairs.txt', 'utf8');
+
+const n = assignmentPairsText
+    .split('\n')
+    .map((line) => line.split(','))
+    .map(([first, second]) => {
+        const [firstStart, firstEnd] = first.split('-');
+        const [secondStart, secondEnd] = second.split('-');
+        const a = {
+            start: +firstStart,
+            end: +firstEnd,
+        };
+        const b = {
+            start: +secondStart,
+            end: +secondEnd,
+        };
+        return [a, b].sort((a, b) => a.start - b.start);
+    })
+    .filter(([a, b]) => {
+        if (a.start === b.start) {
+            return true;
+        } else {
+            return a.end >= b.start;
+        }
+    })
+    .length;
+console.log(n)
